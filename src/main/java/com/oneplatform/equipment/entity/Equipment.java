93b80e5e5d0036ec139807c38e5ddf6588dbc470
Muthu Kumarasamy <muthu.kumarasamy@jbhunt.com>
2020-11-17 18:27:20 +0530

    Updated Equipment details in import.sql

--- src/main/java/com/oneplatform/equipment/entity/Equipment.java
+++ src/main/java/com/oneplatform/equipment/entity/Equipment.java
@@ -3,6 +3,7 @@ package com.oneplatform.equipment.entity;
 import lombok.Data;
 
 import javax.persistence.*;
+import java.math.BigDecimal;
 
 @Data
 @Entity
@@ -12,6 +13,26 @@ public class Equipment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer equipmentId;
-
     private String equipmentNumber;
+    private String equipmentPrefix;
+    private String equipmentType;
+    private String businessUnit;
+    private Integer length;
+    private Integer width;
+    private Integer height;
+    private String equipmentCategoryCode;
+    private String equipmentLoadedStatus;
+    private String equipmentOperationalStatus;
+    private String equipmentMaintenanceStatus;
+    private String locationName;
+    private String addressLine1;
+    private String addressLine2;
+    private String city;
+    private String state;
+    private String country;
+    private String zipcode;
+    private String gpsCurrentLocation;
+    private String lastUpdatedGpsLocation;
+    private BigDecimal latitude;
+    private BigDecimal longitude;
 }
